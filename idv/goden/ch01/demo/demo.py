19af2ff63dfc42d8b8c69360291890fb3238e85d
Gordon <goden.incredible@gmail.com>
2025-08-16 02:49:54 +0800

    chapter 1 is done.

---
+++ idv/goden/ch01/demo/demo.py
@@ -0,0 +1,6 @@
+def add_two_number(num1: int, num2: int):
+    return num1 + num2
+
+class Calculator:
+    def add(self, num1: int, num2: int) :
+        return num1 + num2
---
+++ idv/goden/ch01/test/test_demo.py
@@ -0,0 +1,21 @@
+from idv.goden.ch01.demo.demo import add_two_number, Calculator
+
+
+def test_add_two_number():
+    num1 = 5
+    num2 = 6
+
+    result = add_two_number(num1, num2)
+    assert 11 == result
+
+
+class TestCalculator:
+    def test_add(self):
+
+        num1 = 3
+        num2 = 5
+        calc = Calculator()
+
+        result = calc.add(num1, num2)
+
+        assert 11 == result
